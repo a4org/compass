1221f02490cfe631406721df4d328c69817aec8c
Angold-4 <wangold4w@gmail.com>
2022-01-18 17:04:24 +0800

    run shell script

--- src/sheet/Server.js
+++ src/sheet/Server.js
@@ -1,10 +1,20 @@
 const http = require('http');
+const { exec } = require('child_process');
 
 const httpserver = http.createServer();
 
 
 httpserver.on("request", (req, res) => {
     console.log(req);
+    var compass = exec('./compass.sh',
+	    (error, stdout, stderr) => {
+		console.log(stdout);
+		console.log(stderr);
+		if (error !== null) {
+		    console.log(`exec error: ${error}`);
+		}
+	    }
+    );
     res.write("got request!");
     res.end();
 })
