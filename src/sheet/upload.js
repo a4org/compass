0d63b0357b04aa20fa8fb7031d2d8583c33ae9ad
Angold-4 <wangold4w@gmail.com>
2022-01-25 17:47:00 +0800

    upload logic

--- src/sheet/upload.js
+++ src/sheet/upload.js
@@ -22,46 +22,54 @@ const client = new google.auth.JWT(
     ['https://www.googleapis.com/auth/spreadsheets']
 ); // json web token
 
-client.authorize(function(err, tokens){ // call back function
-    if (err) {
-	console.log(err);
-	return;
+
+fs.readFile('line.txt', "utf8", (error, data) => {
+    if (error) {
+	throw error;
     }
-    console.log('Connected');
-    gsrun(client)
-});
-
-async function gsrun(cl) {
-    const gsapi = google.sheets({version:'v4', auth:cl});
-
-    // TODO Define dataArray
-
-    fs.readFile('toutput.csv', "utf8", (error, data) => {
-	if (error) {
-	    throw error;
+    line = data.toString(); // the line variable
+    client.authorize(function(err, tokens){ // call back function
+	if (err) {
+	    console.log(err);
+	    return;
 	}
-	console.log(data.toString());
-
-	var lines = data.split("\n");
-	var dataArray = [];
-	for (var i = 0; i < lines.length; i++) {
-	    var words = lines[i].split(",");
-	    dataArray.push(words);
-	}
-
-	console.log(dataArray);
-
-	const inputopt = {
-	    spreadsheetId: '11nAOR-7XIAjnhYY-1QuAzKk4xhnAPtq4QoohH6ptXNQ', // test only
-	    // spreadsheetId: '1mvA960mm3QaFyRdwkfIRxhE1UQJl45QEUTnDVxtxiIE',
-	    range: 'Prelim Check!E73',
-	    valueInputOption: 'USER_ENTERED',
-	    resource: {values: dataArray}
-	};
-
-	gsapi.spreadsheets.values.update(inputopt);
+	console.log('Connected');
+	console.log(line);
+	gsrun(client) // download the data
     });
-}
+    
+    async function gsrun(cl) {
+	const gsapi = google.sheets({version:'v4', auth:cl});
 
+	// TODO Define dataArray
+
+	fs.readFile('toutput.csv', "utf8", (error, data) => {
+	    if (error) {
+		throw error;
+	    }
+	    console.log(data.toString());
+
+	    var lines = data.split("\n");
+	    var dataArray = [];
+	    for (var i = 0; i < lines.length; i++) {
+		var words = lines[i].split(",");
+		dataArray.push(words);
+	    }
+
+	    console.log(dataArray);
+
+	    const inputopt = {
+		spreadsheetId: '11nAOR-7XIAjnhYY-1QuAzKk4xhnAPtq4QoohH6ptXNQ', // test only
+		// spreadsheetId: '1mvA960mm3QaFyRdwkfIRxhE1UQJl45QEUTnDVxtxiIE',
+		range: 'Prelim Check!E' + line,
+		valueInputOption: 'USER_ENTERED',
+		resource: {values: dataArray}
+	    };
+
+	    gsapi.spreadsheets.values.update(inputopt);
+	});
+    }
+
+});
 
 
