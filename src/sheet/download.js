9a8808b30d550678b0de30e386257cc14fa89075
Angold-4 <wangold4w@gmail.com>
2022-01-25 17:38:47 +0800

    debug download.js

--- src/sheet/download.js
+++ src/sheet/download.js
@@ -26,66 +26,46 @@ const client = new google.auth.JWT(
 ); // json web token
 
 
-// #1 Get the yesterday line
-
 var line = 0; // line variable (global)
 
-/*
 fs.readFile('line.txt', "utf8", (error, data) => {
     if (error) {
 	throw error;
     }
     line = data.toString(); // the line variable
-});
-*/
-
-client.authorize(function(err, tokens){ // call back function
-    if (err) {
-	console.log(err);
-	return;
-    }
-    console.log('Connected');
-    console.log(line);
-    gsrun(client) // download the data
-});
-
-async function gsrun(cl) {
-    const gsapi = google.sheets({version:'v4', auth:cl});
-
-    const opt = {
-	spreadsheetId: '11nAOR-7XIAjnhYY-1QuAzKk4xhnAPtq4QoohH6ptXNQ', // Test only
-	// spreadsheetId: '1mvA960mm3QaFyRdwkfIRxhE1UQJl45QEUTnDVxtxiIE',
-	range: 'Prelim Check!C135:D2000', // I only need that data today
-    };
-
-    let data = await gsapi.spreadsheets.values.get(opt);
-    let dataArray = data.data.values;
-
-    console.log(dataArray);
-
-    var csv = dataArray.map(function(d){
-	return d.join();
-    }).join('\n');
-
-    // console.log(csv);
-
-    fs.writeFile('sheet.csv', csv, (err) => {
-	console.log(err || "done");
+    client.authorize(function(err, tokens){ // call back function
+	if (err) {
+	    console.log(err);
+	    return;
+	}
+	console.log('Connected');
+	console.log(line);
+	gsrun(client) // download the data
     });
-}
 
+    async function gsrun(cl) {
+	const gsapi = google.sheets({version:'v4', auth:cl});
 
+	const opt = {
+	    spreadsheetId: '11nAOR-7XIAjnhYY-1QuAzKk4xhnAPtq4QoohH6ptXNQ', // Test only
+	    // spreadsheetId: '1mvA960mm3QaFyRdwkfIRxhE1UQJl45QEUTnDVxtxiIE',
+	    range: 'Prelim Check!C' + line + ':D2000', // I only need that data today
+	};
 
+	let data = await gsapi.spreadsheets.values.get(opt);
+	let dataArray = data.data.values;
 
+	console.log(dataArray);
 
+	var csv = dataArray.map(function(d){
+	    return d.join();
+	}).join('\n');
 
+	// console.log(csv);
 
-
-
-
-
-
-
-
-
+	fs.writeFile('sheet.csv', csv, (err) => {
+	    console.log(err || "done");
+	});
+    }
+});
 
