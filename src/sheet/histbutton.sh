92095129be66154588a3f0bb34c6dd66754e6860
Angold-4 <wangold4w@gmail.com>
2022-01-27 00:02:55 +0800

    finish two buttons, now check in server machine

---
+++ src/sheet/histbutton.sh
@@ -0,0 +1,13 @@
+# 2. HistPrices sheet
+
+# 2.1 download today's data
+node histprices/histdown.js
+
+# 2.2 parse them
+./histparser
+
+# 2.3 upload today's data
+node histprices/histup.js
+
+# 2.4 get today's row, which is tomorrow's start index
+node histprices/row.js
--- src/sheet/seqbutton.sh
+++ src/sheet/seqbutton.sh
@@ -15,16 +15,3 @@ node prelimcheck/upload.js
 node prelimcheck/checklines.js
 
 
-# 2. HistPrices sheet
-
-# 2.1 download today's data
-node histprices/histdown.js
-
-# 2.2 parse them
-./histparser
-
-# 2.3 upload today's data
-node histprices/histup.js
-
-# 2.4 get today's row, which is tomorrow's start index
-node histprices/row.js
