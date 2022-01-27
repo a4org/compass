7796db8eb9da936d1ee451ccee3b81493c0b321c
Angold-4 <wangold4w@gmail.com>
2022-01-28 00:18:37 +0800

    delete index file

--- index/README.md
+++
@@ -1,3 +0,0 @@
-# cruel-board-ui
-
-Please visit https://haozihong.github.io/cruel-board-ui/ or https://rank.cruelcoding.com/
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  publicPath: './',
-  configureWebpack: {
-    externals: [{
-      './cptable': 'var cptable'
-    }]
-  }
-}
