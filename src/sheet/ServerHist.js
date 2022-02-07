c843b7860e4faf7ccb2da36206b6acbff6b58af8
Angold-4 <wangold4w@gmail.com>
2022-02-07 23:11:24 +0800

    Fix Twice bug

--- src/sheet/ServerHist.js
+++ src/sheet/ServerHist.js
@@ -4,17 +4,20 @@ const { exec } = require('child_process');
 const httpserver = http.createServer();
 
 httpserver.on("request", (req, res) => {
-    var compass = exec('./histbutton.sh',
-	    (error, stdout, stderr) => {
-		res.write(stdout);
-		console.log(stdout);
-		console.log(stderr);
-		if (error !== null) {
-		    console.log(`exec error: ${error}`);
+    console.log(req.url);
+    if (req.url != '/favicon.ico') {
+	var compass = exec('./histbutton.sh',
+		(error, stdout, stderr) => {
+		    res.write(stdout);
+		    console.log(stdout);
+		    console.log(stderr);
+		    if (error !== null) {
+			console.log(`exec error: ${error}`);
+		    }
+		    res.end();
 		}
-		res.end();
-	    }
-    );
+	);
+    }
 })
 
 httpserver.on("connection", connection=> {
