3520a7b36f1241ee729bad3d294cbbb974b891b1
Angold-4 <wangold4w@gmail.com>
2022-02-13 15:56:45 +0800

    add ServerCurr

---
+++ src/sheet/ServerCurr.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const { exec } = require('child_process');
+
+const httpserver = http.createServer();
+
+httpserver.on("request", (req, res) => {
+    if (req.url != '/favicon.ico') {
+	var compass = exec('./currbutton.sh',
+		(error, stdout, stderr) => {
+		    res.write(stdout);
+		    console.log(stdout);
+		    console.log(stderr);
+		    if (error !== null) {
+			console.log(`exec error: ${error}`);
+		    }
+		    res.end();
+		}
+	);
+    }
+})
+
+httpserver.on("connection", connection=> {
+    console.log("Someone just connected!")
+})
+
+httpserver.listen(7123, ()=>console.log("Listenng on port 7123"));
