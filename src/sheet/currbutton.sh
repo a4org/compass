eeed26da6322b232a30eef343b3e4d1cd4671981
Angold-4 <wangold4w@gmail.com>
2022-02-13 15:52:51 +0800

    curr test button logic

---
+++ src/sheet/currbutton.sh
@@ -0,0 +1,13 @@
+# 3. Form Responses sheet
+
+node currency/currline.js
+
+# 2.1 download today's data
+node currency/currdown.js
+
+# 2.2 parse them
+./currparser
+
+# 2.3 upload today's data
+node currency/currup.js
+
